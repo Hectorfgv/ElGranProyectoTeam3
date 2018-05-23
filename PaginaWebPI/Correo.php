f772a15769b7770c075a41992c685bfd07340227
JoanVergara <eljohanvergara99@gmail.com>
2018-05-23 18:25:23 +0200

    Añado contenido a pagina web

--- PaginaWebPI/Correo.php
+++ PaginaWebPI/Correo.php
@@ -1,10 +1,11 @@
 <?php
   $destino= "joanvergara99@gmail.com";
+
   $nombre = $_REQUEST["name"];
-  $correo = $_REQUEST["correo"];
-  $mensaje = $_REQUEST["mensaje"];
+  $correo = $_REQUEST["email"];
+  $mensaje= $_REQUEST["message"];
   $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nmensaje: " . $mensaje;
 
-  mail($destino, "Contacto", $contenido);
+  mail($destino, $nombre, $contenido);
 
 ?>
